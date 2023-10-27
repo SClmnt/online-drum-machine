a2a23c71dc41687e942488856b8b67735f3776c9
Clément Silone <silone.clement@gmail.com>
2023-10-28 00:23:23 +0200

    Fix public url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Home from './pages/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>         
